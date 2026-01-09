6509eaef5cee8318d8c16f74b0b070bbef623dab
madhu mathi <tsuji6607@gmail.com>
2026-01-09 22:39:36 +0530

    Started the exclusive section

--- src/pages/Home/Exclusive.jsx
+++ src/pages/Home/Exclusive.jsx
@@ -1,7 +1,18 @@
 export default function Exclusive(){
+    const offers = [
+        {name:"Hotel Offer",
+            description:"Get 20% off on your next stay!",
+            image:"/images/hotel-offer.jpg"
+        }
+    ]
     return(
         <div>
             <h1>Exclusive offers</h1>
+            <div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
